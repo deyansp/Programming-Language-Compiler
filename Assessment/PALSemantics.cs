69d79f52581671341b7f06ff3452dbd881ea744d
Deyan <43583063+deyansp@users.noreply.github.com>
2021-04-16 22:40:30 +0100

    Create PALSemantics.cs

---
+++ Assessment/PALSemantics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AllanMilne.Ardkit;
+
+namespace AllanMilne.PALCompiler
+{
+    class PALSemantics : Semantics
+    {
+        public PALSemantics(IParser p) : base(p)
+        { }
+        enum PALType
+        {
+           Boolean,
+           Integer,
+           Real,
+           Invalid
+        }
+
+        struct Declaration
+        {
+            PALType varType;
+
+        }
+
+        Dictionary<String, PALType> variables;
+    }
+}
