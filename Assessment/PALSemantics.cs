f2699d6a8b15a35175c0a24739b59c7a1a74b020
Deyan <43583063+deyansp@users.noreply.github.com>
2021-04-23 17:49:24 +0100

    Update PALSemantics.cs

--- Assessment/PALSemantics.cs
+++ Assessment/PALSemantics.cs
@@ -10,59 +10,50 @@ namespace AllanMilne.PALCompiler
         public PALSemantics(IParser p) : base(p)
         { }
 
-		// stores newly defined variables in the current Scope object's symbols
-		public void declareVariable(IToken id, int type)
-		{
-			if (!id.Is(Token.IdentifierToken)) return;  // only proceed if an identifier.
-			Scope symbols = Scope.CurrentScope;
-			if (symbols.IsDefined(id.TokenValue))
-			{
-				semanticError(new AlreadyDeclaredError(id, symbols.Get(id.TokenValue)));
-			}
-			else
-			{
-				symbols.Add(new VarSymbol(id, type));
-			}
-		}
-
-		public bool variableExists(IToken id)
+        // stores newly defined variables in the current Scope's symbols table
+        public void declareVariable(IToken id, int type)
         {
-			Scope symbols = Scope.CurrentScope;
-			return symbols.IsDefined(id.TokenValue);
-		}
+            if (!id.Is(Token.IdentifierToken)) return;  // only proceed if an identifier.
+            Scope symbols = Scope.CurrentScope;
+            if (symbols.IsDefined(id.TokenValue))
+            {
+                semanticError(new AlreadyDeclaredError(id, symbols.Get(id.TokenValue)));
+            }
+            else
+            {
+                symbols.Add(new VarSymbol(id, type));
+            }
+        }
 
-		// Check the usage of an identifier.
-		public int checkVariable(IToken id)
-		{
-			if (!id.Is(Token.IdentifierToken)) return;  // only proceed if we have an identifier.
-			if (!Scope.CurrentScope.IsDefined(id.TokenValue))
-			{
-				semanticError(new NotDeclaredError(id));
-			}
-			else checkValueType(id); // check type compatibility
-		}
+        public bool variableExists(IToken id)
+        {
+            Scope symbols = Scope.CurrentScope;
+            return symbols.IsDefined(id.TokenValue);
+        }
 
-		public int checkValueType(IToken token)
-		{
-			int thisType = LanguageType.Undefined;
-			if (token.Is(Token.IdentifierToken))
-				thisType = Scope.CurrentScope.Get(token.TokenValue).Type;
-			else if (token.Is(Token.IntegerToken))
-				thisType = LanguageType.Integer;
-			else if (token.Is(Token.RealToken))
-				thisType = LanguageType.Real;
-			else
-				thisType = LanguageType.Undefined;
+        // Check if a variable is defined in the current scope and return its type
+        public int checkVariable(IToken id)
+        {
+            if (!Scope.CurrentScope.IsDefined(id.TokenValue))
+            {
+                semanticError(new NotDeclaredError(id));
+                return LanguageType.Undefined;
+            }
+            else return checkValueType(id);
+        }
 
-			return thisType;
-			
-			/*// if not already set then set the current type being processed
-			if (currentType == LanguageType.Undefined)
-				currentType = thisType;
-			if (currentType != thisType)
-			{
-				semanticError(new TypeConflictError(token, thisType, currentType));
-			}*/
-		}
-	}
+        public int checkValueType(IToken token)
+        {
+            int thisType = LanguageType.Undefined;
+
+            if (token.Is(Token.IdentifierToken))
+                thisType = Scope.CurrentScope.Get(token.TokenValue).Type;
+            else if (token.Is(Token.IntegerToken))
+                thisType = LanguageType.Integer;
+            else if (token.Is(Token.RealToken))
+                thisType = LanguageType.Real;
+
+            return thisType;
+        }
+    }
 }
