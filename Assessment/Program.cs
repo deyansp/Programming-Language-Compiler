69cd2d0da1d72591bb8303419b24ce3d2d2ebcbf
Deyan <43583063+deyansp@users.noreply.github.com>
2021-05-10 15:52:03 +0100

    added new test mode to compiler

--- Assessment/Program.cs
+++ Assessment/Program.cs
@@ -15,19 +15,58 @@ namespace AllanMilne.PALCompiler
                 return;
             }
 
-            //--- Open the input source file.
+            // compiler test mode
+            else if (args[0] == "TXT_FILES_TEST")
+            {
+                runTestsFromTxtFiles();
+            }
+
+            // otherwise compile the file provided as an argument
+            else
+                runCompiler(args[0]);
+
+        } // end Main method.
+        
+        static void runTestsFromTxtFiles()
+        {
+            // getting the root project directory
+            DirectoryInfo baseDir = new DirectoryInfo(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName);
+            
+            // the folder with code txt files
+            var testDir = baseDir.GetDirectories("Tests");
+
+            if (testDir.Length != 1)
+            {
+                Console.WriteLine("Unable to locate 'Tests' folder");
+                return;
+            }
+
+            // storing all txt file paths in a list
+            List<FileInfo> testFiles = new List<FileInfo>(testDir[0].EnumerateFiles("*.txt", SearchOption.AllDirectories));
+
+            // checking each file
+            foreach(var test in testFiles)
+            {
+                Console.WriteLine("\nFile: " + test.Name);
+                runCompiler(test.FullName);
+            }
+        }
+        
+        static void runCompiler(string filePath)
+        {
+            // Open the input source file.
             StreamReader infile = null;
             try
             {
-                infile = new StreamReader(args[0]);
+                infile = new StreamReader(filePath);
             }
             catch (IOException e) { Console.WriteLine(e.ToString()); }
 
-            //--- Parsing and then outputting any syntax errors found
+            // Parsing and then outputting any errors found
             PALParser parser = new PALParser();
             parser.Parse(infile);
 
-            foreach(var err in parser.Errors)
+            foreach (var err in parser.Errors)
             {
                 Console.WriteLine(err.ToString());
             }
@@ -37,8 +76,7 @@ namespace AllanMilne.PALCompiler
                 infile.Close();
             }
             catch (IOException e) { Console.WriteLine(e.ToString()); }
-
-        } // end Main method.
+        }
     }
 }
 
